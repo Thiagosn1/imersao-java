3e88ee84c460781c97ec6f843d1675078d08841a
Thiago Nascimento <70329966+Thiagosn1@users.noreply.github.com>
2022-07-19 19:01:43 -0300

    Aula 02

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -6,7 +8,9 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
+
 public class App {
+
     public static void main(String[] args) throws Exception {
         // Fazer uma conexão HTTP e buscar os tops 250 filmes
         String url = "https://alura-filmes.herokuapp.com/conteudos";
@@ -21,10 +25,18 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         
         // Exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
+
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
     }
